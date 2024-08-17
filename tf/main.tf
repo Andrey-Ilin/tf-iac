1ffa92c09f8adf91e58ff450b822b47c186e8ba1
Andrii Ilin <andrey.ilyin83@gmail.com>
2024-08-17 11:48:46 -0600

    feat(tf): add initial setup

---
+++ tf/main.tf
@@ -0,0 +1,8 @@
+
+module "gke_cluster" {
+  source         = "github.com/andrey-ilin/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
+}
+  
